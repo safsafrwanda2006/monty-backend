f0f16d5789069f29a3af165acce0c94cb3bc51fa
Mustafa Khamis <safsafrwanda2006@gmail.com>
2026-05-07 20:52:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
