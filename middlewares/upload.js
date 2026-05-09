1a1f519531e324fea294177fff80c213f43b5afd
Mustafa Khamis <safsafrwanda2006@gmail.com>
2026-05-09 11:54:34 +0200

    Change multer storage to memory storage

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,21 +1,6 @@
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-
-  filename: function (req, file, cb) {
-    const uniqueName =
-      Date.now() +
-      "-" +
-      Math.round(Math.random() * 1e9) +
-      path.extname(file.originalname);
-
-    cb(null, uniqueName);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
